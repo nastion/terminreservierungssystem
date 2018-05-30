e7ed66076890a6c77a15fc85e85e3da9d7c63307
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-05-30 15:42:13 +0200

    perstist event

--- Java/src/main/java/controller/EventController.java
+++ Java/src/main/java/controller/EventController.java
@@ -26,22 +26,27 @@ public class EventController {
 
 	public void removePeople(Event evt, User teilnehmer) {
 		evt.remTeilnehmer(teilnehmer);
+		controller.getSession().persist(evt);
 	}
 
 	public void sendNotification(Event evt) {
-		evt.notifyTeilnehmer();
+    	evt.notifyTeilnehmer();
+		controller.getSession().persist(evt);
 	}
 
 	public void addDescription(Poll evt, String description) {
 		evt.setDescripition(description);
+		controller.getSession().persist(evt);
 	}
 
 	public void addLocation(Poll evt, String location) {
 		evt.setLocation(location);
+		controller.getSession().persist(evt);
 	}
 
 	public void setDate(Poll evt, Date date) {
 		evt.setDate(date);
+		controller.getSession().persist(evt);
 	}
 
 }
