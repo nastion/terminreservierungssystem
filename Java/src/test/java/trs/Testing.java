de7b93178c35e46109fa07f20e9ed1d11ee5183b
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-06-08 00:37:54 +0200

    Test comments

--- Java/src/test/java/trs/Testing.java
+++ Java/src/test/java/trs/Testing.java
@@ -25,18 +25,24 @@ public class Testing {
         this.controller.getPollRepo().deleteAll();
         this.controller.getUserRepo().deleteAll();
     }
-
+/*
+    Test: createUser returns true if user is created
+ */
     @Test
     public void createUser(){
         Assert.assertTrue(controller.getUserController().createUser("TestUser", "TestPw"));
     }
-
+/*
+    Test: User has to have a unique username, so if there is already a user with this particular username, createUser should return false
+ */
     @Test
     public void createSameUserTwice(){
         controller.getUserController().createUser("TestUserTwice","TestPw");
         Assert.assertFalse(controller.getUserController().createUser("TestUserTwice", "TestPw"));
     }
-
+/*
+    Test: Creates a user and makes a login, the current user should have the same username like the created user
+ */
     @Test
     public void getUserNameCurrentUser() {
         User user = new User("getUserName", "TestPw");
@@ -44,7 +50,9 @@ public class Testing {
         controller.getUserController().login(user.getName(), user.getPassword());
         Assert.assertEquals("getUserName", controller.getCurrentUser().getName());
     }
-
+/*
+    Test: Creates a user and makes a login, the current user should have the same password like the created user
+*/
     @Test
     public void getPasswordCurrentUser() {
         User user = new User("getUserPassword", "TestPw");
@@ -53,7 +61,10 @@ public class Testing {
         Assert.assertEquals("TestPw", controller.getCurrentUser().getPassword());
     }
 
-
+/*
+    Test: Creates a user and searches for this user. searchUser returns a User and the method getName returns the Username, searchUser(Username).
+    the returned name should be the same like the username from the created user
+ */
     @Test
     public void createUserAndSearchAssertSameName(){
         User user = new User("TestFindUserName", "TestPw");
@@ -61,13 +72,19 @@ public class Testing {
         Assert.assertEquals("TestFindUserName", controller.getUserController().searchUser("TestFindUserName").getName());
     }
 
-    @Test
+    /*
+        Test: Creates a user and searches for this user. searchUser returns a User and the method getPassword returns the password, searchUser(Username).
+        the returned password should be the same like the password from the created user
+     */    @Test
     public void createUserAndSearchAssertSamePassword(){
         User user = new User("TestFindUserPassword", "TestPw");
         controller.getUserController().createUser(user);
         Assert.assertEquals("TestPw", controller.getUserController().searchUser("TestFindUserPassword").getPassword());
     }
-
+/*
+    Test: Creates a new user and makes a login, login checks if this username exists and compares the entered password with the existing user.
+    If the username and password exist, a currentUser is going to be set.
+ */
     @Test
     public void loginUser(){
         User testUser = new User("TestUser", "TestPw");
@@ -76,19 +93,26 @@ public class Testing {
         Assert.assertEquals(testUser, controller.getCurrentUser());
     }
 
+    /*
+        Test: Creates a new user and makes a login, login checks if this username exists and compares the entered password with the existing user.
+        If the password doesent fit with the user, currentUser is going to be set as NULL.
+     */
     @Test
     public void loginNotExistingUser(){
         controller.getUserController().login("Abc","TestPw");
         Assert.assertNull(controller.getCurrentUser());
     }
-
+/*
+    Test: creates a User and makes a login, logout sets the currentUser to null
+ */
     @Test
     public void logoutUser(){
         User testUser = new User("LogoutUser", "TestPw");
         controller.getUserController().createUser(testUser);
         controller.getUserController().login(testUser.getName(),testUser.getPassword());
+        controller.getUserController().logout();
         Assert.assertNull(controller.getCurrentUser());
     }
 
-    
+
 }
