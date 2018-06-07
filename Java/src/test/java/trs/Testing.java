820d64a9d602226b466eca1fb273da06e3330b1b
afreudenthaler-tgm <afreudenthaler@student.tgm.ac.at>
2018-06-07 22:29:47 +0200

    createUser, createSameUserTwice, createUserAndSearch

--- Java/src/test/java/trs/Testing.java
+++ Java/src/test/java/trs/Testing.java
@@ -1,11 +1,14 @@
 package trs;
 
+import example.data.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import trs.controller.Controller;
+import trs.controller.UserController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,5 +20,32 @@ public class Testing {
     public void contextLoads() throws Exception {
         
     }
+
+    @Test
+    public void createUser(){
+        Controller c = new Controller();
+        UserController uc = new UserController(c);
+        uc.createUser("TestUser","TestPw");
+        Assert.assertEquals(true, uc.createUser("TestUser","TestPw"));
+    }
+
+    @Test
+    public void createSameUserTwice(){
+        Controller c = new Controller();
+        UserController uc = new UserController(c);
+        uc.createUser("TestUser","TestPw");
+        uc.createUser("TestUser","TestPw");
+        Assert.assertEquals(false, uc.createUser("TestUser","TestPw"));
+    }
+
+    @Test
+    public void createUserAndSearch(){
+        Controller c = new Controller();
+        UserController uc = new UserController(c);
+        uc.createUser("TestUser","TestPw");
+        Assert.assertEquals("TestUser", uc.searchUser("TestUser"));
+    }
+
+
 
 }
