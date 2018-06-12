a5e1d7a6bfa100df5197f9f60d13a4fed5fdb1ab
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2018-06-12 21:27:23 +0200

    added comments for eventResult testcases

--- Java/src/main/resources/test/cypress/integration/eventResult_test.js
+++ Java/src/main/resources/test/cypress/integration/eventResult_test.js
@@ -1,8 +1,18 @@
+/*
+    Diese Testcases prüfen die Result Seite
+*/
 describe('Event Result Page', function() {
+    
+    /*
+        Dieser Testcase ruft die Seite auf
+    */
     it('Event Result aufrufen', function() {
       cy.visit('http://localhost/')             //URL muss noch geändert werden
     })
 
+    /*
+        Dieser Testcase prüft ob die Überschrift vorhanden ist
+    */
     it('Event Result Ueberschriften pruefen', function() {
         cy.visit('http://localhost/')                   //URL richtig einfügen
 
