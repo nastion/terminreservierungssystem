8d26fefb661905798c50746f1355706df938029a
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2018-06-12 21:48:06 +0200

    added comments for sample testcases

--- Java/src/main/resources/test/cypress/integration/simple_first_test.js
+++ Java/src/main/resources/test/cypress/integration/simple_first_test.js
@@ -1,4 +1,11 @@
+/*
+  Diese Testcases dienen nur zur veranschaulichung der Funktionsweise von Cypress-Tests
+*/
 describe('My First Test', function() {
+  
+  /*
+    Dieser Testcase ruft die Seite auf und prüft ob ein bestimmtes Element vorhanden ist
+  */
   it('Does not do much!', function() {
     cy.visit('https://example.cypress.io')
     cy.contains('your')
