0770c89f13b1fc0ba7b2ac694017d6dd50594117
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2018-06-12 21:46:18 +0200

    added comments for signin page testcases

--- Java/src/main/resources/test/cypress/integration/signIn_test.js
+++ Java/src/main/resources/test/cypress/integration/signIn_test.js
@@ -1,8 +1,18 @@
+/*
+    Diese Testcases prüfen die Signin Seite
+*/
 describe('Signin Page', function() {
+    
+    /*
+        Dieser Testcase ruft die Seite auf
+    */
     it('Loginpage aufrufen', function() {
       cy.visit('http://localhost:8080/signin')    //URL richtig einfügen
     })
 
+    /*
+        Dieser Testcase prüft ob die Überschrift vorhanden ist
+    */
     it('Loginpage Ueberschrift pruefen', function() {
         cy.visit('http://localhost:8080/')                   //URL richtig einfügen
 
@@ -10,6 +20,9 @@ describe('Signin Page', function() {
         cy.contains('Anmeldung')
       })
 
+    /*
+        Dieser Testcase prüft ob die Inputfelder vorhanden sind und schreibt in diese etwas hinein
+    */
       it('Loginpage Inputfelder pruefen', function() {
         cy.visit('http://localhost:8080/')      //URL richtig einfügen
 
@@ -20,6 +33,9 @@ describe('Signin Page', function() {
         cy.get('.input-sm').type('1233').should('have.value', '1233')   //hier muss die Klasse input-sm noch geändert werden
       })
 
+    /*
+        Dieser Testcase prüft ob die Buttons vorhanden sind und klickt diese
+    */
       it('Loginpage Buttons pruefen', function() {
         cy.visit('http://localhost:8080/')    //URL richtig einfügen
 
